16102585086d8f06581315c7643fe9bc3ca58479
Sagar <sagarwaghmare1384@gmail.com>
2024-10-30 17:25:20 +0530

    Fix all erros

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 // require('dotenv').config({path:'./env'})
 
 import dotenv from "dotenv"
+import express from "express";
 import connectDB from "./db/index.js";
+
 dotenv.config({
     path:'./env'
 })
+
+const app = express();
 
 
 connectDB()
 .then(()=>{
   app.listen(process.env.PORT||8000,()=>{
     console.log(`Server is running at port :${process.env.PORT}`);
+  })
+  app.get("/",(req,res)=>{
+    return res.send("Server is running");
   })
 })
 .catch((error)=>{
